5054327b995a1677a16f99fb38bdcb1481b98fc4
Sebastien Rouif <sebastien.rouif@monitisecreate.com>
2015-01-09 15:42:00 +0000

    Basic DrawerApp

---
+++ app/src/live/java/com/monitisecreate/responsiveDesign/Config.java
@@ -0,0 +1,9 @@
+package com.monitisecreate.responsiveDesign;
+
+/**
+ * Contains flavor configuration
+ */
+public class Config {
+
+    public static final boolean IS_LIVE = true;
+}
---
+++ app/src/main/java/com/monitisecreate/responsiveDesign/ExampleApplication.java
@@ -0,0 +1,15 @@
+package com.monitisecreate.responsiveDesign;
+
+import android.app.Application;
+
+/**
+ * Add in cross application code here. Could provide access to API etc.
+ */
+public class ExampleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
